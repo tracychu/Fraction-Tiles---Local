2637b0670cfa5532defc675e20ba6d65e2339920
tracychu <tracyschu@gmail.com>
2015-11-01 13:29:40 -0800

    Initial Commit

---
+++ Fraction Tiles - LocalUITests/Fraction_Tiles___LocalUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Fraction_Tiles___LocalUITests.swift
+//  Fraction Tiles - LocalUITests
+//
+//  Created by Tracy Chu on 11/1/15.
+//  Copyright © 2015 Tracy Chu. All rights reserved.
+//
+
+import XCTest
+
+class Fraction_Tiles___LocalUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
